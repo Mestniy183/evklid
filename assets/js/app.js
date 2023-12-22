2042d86a5ac3fdc3d2cbd5dea19bd28bb25b5b6d
Mestniy183 <ken-zero@ya.ru>
2023-12-22 11:29:24 +0300

    Начали делать сайт

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+// let a = 1
+// function b () {
+// let c = 2 
+
+// }
+// console.log(a)
+// console.log(c)
+ 
+// let num1 = 11;
+// let num2 = 22;
+// let result = num1 + num2;
+// console.log(result);
+
+// let name = 'сергей';
+// let name2 = name;
+// console.log(name2);
+
+// let name;
+// let admin;
+//  name = 'Джон';
+//  admin = name;
+
+// console.log(admin);
+
+// let nam = prompt('Введите своё имя')
+let age = '45'
+let nam1 = Number(age)
+alert( nam1)
+// console.log(typeof nam);
+// confirm(`Меня зовут ${nam} и мне ${age} лет`);
